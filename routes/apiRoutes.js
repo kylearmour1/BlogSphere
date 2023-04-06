67119a85a942213d06855a75a4837a002b3ba66f
Thor the Fixer <120442441+Jaymasters440@users.noreply.github.com>
2023-04-06 15:19:30 -0800

    Add post to posts and comments.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,84 +1,110 @@
 const router = require('express').Router();
 const { User, Post, Comment, Like } = require('../models');
 const withAuth = require('../utils/auth');
-const { logout } = require ('../models/logout')
-
 
 
 router.post('/users', async (req, res) => {
-    try {
-        const userData = await User.create(req.body);
-    
-        req.session.save(() => {
-          req.session.user_id = userData.id;
-          req.session.logged_in = true;
-    
-          res.status(200).json(userData);
-        });
-      } catch (err) {
-        res.status(400).json(err);
-      }
+  try {
+    const userData = await User.create(req.body);
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
+
 
 router.post('/login', async (req, res) => {
-    try {
-        const userData = await User.findOne({ where: { email: req.body.email } });
-    
-        if (!userData) {
-          res
-            .status(400)
-            .json({ message: 'Incorrect email or password, please try again' });
-          return;
-        }
-    
-        const validPassword = await userData.checkPassword(req.body.password);
-    
-        if (!validPassword) {
-          res
-            .status(400)
-            .json({ message: 'Incorrect email or password, please try again' });
-          return;
-        }
-    
-        req.session.save(() => {
-          req.session.user_id = userData.id;
-          req.session.logged_in = true;
-          
-          res.json({ user: userData, message: 'You are now logged in!' });
-        });
-    
-      } catch (err) {
-        res.status(400).json(err);
-      }
-});
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
-router.post('/posts', withAuth, async (req, res) => {
+    if (!userData) {
+      res.status(400).json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
 
-});
+    const validPassword = await userData.checkPassword(req.body.password);
 
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
 
-router.get('/posts', async (req, res) => {
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
 
-});
-
-
-router.post('/comments', withAuth, async (req, res) => {
-
-});
-
-
-router.post('/likes', withAuth, async (req, res) => {
-
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 
 router.post('/logout', (req, res) => {
-    if (req.session.logged_in) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
-    }
-  });
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+
+router.post('/posts', withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+router.get('/posts', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [{ model: User }, { model: Comment }],
+      
+    });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+router.post('/comments', withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+router.post('/likes', withAuth, async (req, res) => {
+  try {
+    const newLike = await Like.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newLike);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
