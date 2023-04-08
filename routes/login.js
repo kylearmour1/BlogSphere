ae8858bc5062a7ed025d944d18162dcd1757c211
kylearmour1 <kylejarmour@gmail.com>
2023-04-08 10:22:58 -0700

    git

--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,12 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
-  // Collect values from the login form
+
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
-    // Send a POST request to the API endpoint
+
     const response = await fetch('/api/user/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
@@ -14,7 +14,7 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      // If successful, redirect the browser to the main page
+     
       document.location.replace('/main');
     } else {
       alert(response.statusText);
@@ -51,3 +51,7 @@ document
 document
   .querySelector('.signup-form')
   .addEventListener('submit', signupFormHandler);
+
+
+
+  
