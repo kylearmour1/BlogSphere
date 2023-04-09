6964443c67786bcb1f19233d15eaf9f3c8fd9eac
linhntran <tran.linhn@gmail.com>
2023-04-09 09:31:24 -0700

    update with comments working

--- controllers/api/like.js
+++ controllers/api/like.js
@@ -3,13 +3,13 @@ const { Like } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/likes', withAuth, async (req, res) => {
-    try {
-      const newLike = await Like.create({
-        ...req.body,
-        user_id: req.session.user_id,
-      });
-      res.status(200).json(newLike);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+  try {
+    const newLike = await Like.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newLike);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
