defe0764c1b8ccecea6500bfe8ff430aeef01668
Thor the Fixer <120442441+Jaymasters440@users.noreply.github.com>
2023-04-11 18:31:20 -0800

    Add "withAuth" to line  19 of user.js  controllers

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -15,16 +15,18 @@ router.post('/users', async (req, res) => {
   }
 });
 
-router.get('/', (req, res) => {
-  User.findAll({
-    attributes: { exclude: ['password'] },
-  })
-    .then((dbUserData) => res.json(dbUserData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+router.post('/', withAuth, async (req, res) => {
+  try {    
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
     });
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
+
 
 router.post('/', withAuth, async (req, res) => {
   try {
