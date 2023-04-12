626db124fc2f35e6d42bc8dfe345c6f7b784df97
linhntran <tran.linhn@gmail.com>
2023-04-11 20:32:09 -0700

    removed duplicate line in post

--- controllers/api/post.js
+++ controllers/api/post.js
@@ -1,9 +1,5 @@
 const router = require('express').Router();
-
 const { Post, User, Comment } = require('../../models');
-
-const { Post, Comment } = require('../../models');
-
 const withAuth = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
